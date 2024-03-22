6129c7388a6494064860bfc8ec385ce4e9d7d8db
Milan Mraz <Milan.Mraz@leuze.com>
2024-03-22 11:50:35 +0100

    Bugfix: Phidget driver could not be started properly; Add a default laser scan "scan1" into the launch file of the Description package

--- leuze_phidget_driver/src/leuze_phidget_driver_node.cpp
+++ leuze_phidget_driver/src/leuze_phidget_driver_node.cpp
@@ -1,15 +1,15 @@
 #include "leuze_phidget_driver.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);  //ToDo: ROS1 uses the init option rclcpp::init_options::AnonymousName). This is currently not supported in the ROS2 driver
+  rclcpp::init(argc, argv);    //ToDo: ROS1 uses the init option rclcpp::init_options::AnonymousName). This is currently not supported in the ROS2 driver
 
-    sleep(3); //Wait for phidgets_ik_node to come up and spawn its topics
-    
-    auto node = std::make_shared<LeuzePhidgetDriver>();
+  sleep(3);   //Wait for phidgets_ik_node to come up and spawn its topics
 
-    rclcpp::spin(node); //Pause the program here and keep the node alive until we press CTRL+C
+  auto node = std::make_shared<LeuzePhidgetDriver>();
+  node->init();
+  rclcpp::spin(node);   //Pause the program here and keep the node alive until we press CTRL+C
 
-    rclcpp::shutdown();  //Stop ROS2 communications
-    return 0;
+  rclcpp::shutdown();    //Stop ROS2 communications
+  return 0;
 }
